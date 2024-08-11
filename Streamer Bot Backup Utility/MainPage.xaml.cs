2383e57e05e9a8d5b59190a2df3987c35186224e
Peter Taylor <peter@taylorsnet.co.uk>
2024-08-11 20:50:49 +0100

    Feature: Added ability to select the streamerbot.exe location

--- Streamer Bot Backup Utility/MainPage.xaml.cs
+++ Streamer Bot Backup Utility/MainPage.xaml.cs
@@ -1,25 +1,76 @@
-﻿namespace Streamer_Bot_Backup_Utility
+﻿
+namespace Streamer_Bot_Backup_Utility
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        /// <summary>
+        /// The configuration for the app
+        /// </summary>
+        public Model.Config Config { get; set; } = new();
 
         public MainPage()
         {
             InitializeComponent();
+
+            try
+            {
+                /// LOAD CONFIG
+                string strConfig = Preferences.Get("config", string.Empty);
+                Model.Config? tmpConfig = null;
+                if (!string.IsNullOrWhiteSpace(strConfig)) { tmpConfig = Newtonsoft.Json.JsonConvert.DeserializeObject<Model.Config>(strConfig); }
+                if (tmpConfig == null) { this.Config = new(); } else { this.Config = tmpConfig; }
+                /// LOAD CONFIG
+            }
+            catch (Exception ex) { DisplayAlert(Streamer_Bot_Backup_Utility.Resources.Strings.Localisation.Error, ex.Message, Streamer_Bot_Backup_Utility.Resources.Strings.Localisation.OK); }
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        /// <summary>
+        /// Page Loaded
+        /// </summary>
+        private void ContentPage_Loaded(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            try
+            {
+                this.BindingContext = this;
+            }
+            catch (Exception ex) { DisplayAlert(Streamer_Bot_Backup_Utility.Resources.Strings.Localisation.Error, ex.Message, Streamer_Bot_Backup_Utility.Resources.Strings.Localisation.OK); }
         }
-    }
 
+        /// <summary>
+        /// Page Closing
+        /// </summary>
+        private void ContentPage_Unloaded(object sender, EventArgs e)
+        {
+            try
+            {
+                Preferences.Set("config", Newtonsoft.Json.JsonConvert.SerializeObject(this.Config));
+            }
+            catch (Exception ex) { DisplayAlert(Streamer_Bot_Backup_Utility.Resources.Strings.Localisation.Error, ex.Message, Streamer_Bot_Backup_Utility.Resources.Strings.Localisation.OK); }
+        }
+
+        /// <summary>
+        /// Select the Streamer Bot exe file location
+        /// </summary>
+        private async void btnStreamerBotLocation_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                PickOptions options = new()
+                {
+                    PickerTitle = "asd",
+                    FileTypes = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>> {
+                        { DevicePlatform.WinUI, new[] {".exe" } }
+                    })
+                };
+
+                var result = await FilePicker.Default.PickAsync(options);
+                if ((result == null)) { return; } // File not selected
+                if (!System.IO.File.Exists(result.FullPath)) { return; } // File not found
+                this.Config.StreamerBotLocation = Path.GetFullPath(result.FullPath);
+                OnPropertyChanged(nameof(Config));
+            }
+            catch (Exception ex) { await DisplayAlert(Streamer_Bot_Backup_Utility.Resources.Strings.Localisation.Error, ex.Message, Streamer_Bot_Backup_Utility.Resources.Strings.Localisation.OK); }
+        }
+
+    }
 }
---
+++ Streamer Bot Backup Utility/Model/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streamer_Bot_Backup_Utility.Model
+{
+    public class Config
+    {
+        /// <summary>
+        /// The directory containing the Streamer Bot exe file
+        /// </summary>
+        public string StreamerBotLocation { get; set; } = Streamer_Bot_Backup_Utility.Resources.Strings.Localisation.not_located;
+
+    }
+}
